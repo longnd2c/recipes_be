be658255f295244ff86613e782e91ef51d941da8
Duy Long <longnd2c@gmail.com>
2023-08-17 10:28:23 +0700

    add vercel

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const sequelize = require('./database')
 const RecipeRoutes = require("./src/routes/RecipeRoutes");
 
@@ -8,12 +7,8 @@ sequelize.sync().then(() => console.log("db is ready"))
 
 app.use(express.json());
 
-app.listen(8000, () => {
+app.listen(8080, () => {
     console.log("app is running");
 });
-
-app.use(cors({
-    origin: '*'
-}));
 
 app.use("/api", RecipeRoutes);
